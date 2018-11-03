14312ef251323fd15c18fdbe2e802d43dc52db7f
Adam Hughes <ashughes07@aol.com>
2018-11-03 00:04:07 +0000

    extended on the functions

--- School Register - OOP/menu.cpp
+++ School Register - OOP/menu.cpp
@@ -2,6 +2,13 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+
+Menu::Menu(std::vector<Student>& v) : students(v)
+{
+
+}
 
 void Menu::displayOptions()
 {
@@ -11,7 +18,7 @@ void Menu::displayOptions()
     input.open("B:\\Desktop\\School Register\\displayOptions.txt");
     if (!input)
     {
-        std::cerr << "File not found!";
+        std::cout << "File not found!";
     }
 
     while (!input.eof())
@@ -20,17 +27,53 @@ void Menu::displayOptions()
         std::cout << "\t" << options << "\n";
     }
     input.close();
+    optionSelction();
 }
 
 void Menu::optionSelction()
 {
+    int select;
+    std::cout << "\nPress (1-7): ";
+    std::cin >> select;
 
+    while (select < 1 || select > 7)
+    {
+        std::cout << "Wrong input, try again: ";
+    }
 }
 
-void Menu::addStudent(Student* addStudent)
+void Menu::addStudent(Student* addS)
 {
-    addStudent->enterStudentID();
-    addStudent->enterStudentName();
-    addStudent->enterGroupID();
+    std::cout << std::string(100, '\n');
+
+    int numOfStudents;
+    std::cout << "Enter the amount of students: ";
+    std::cin >> numOfStudents;
+
+    for (int i = 1; i <= numOfStudents; ++i)
+    {
+        std::cout << i << "\t" << "Student ID:\t";
+        addS->enterStudentID();
+        std::cout << i << "\t" << "Student Name:\t";
+        addS->enterStudentName();
+        std::cout << i << "\t" << "Group ID:\t";
+        addS->enterGroupID();
+        std::cout << i << "\t" << "Attendance:\t";
+        addS->enterAttendance();
+
+        students.push_back(*addS);
+        std::cout << "\n\n";
+    }
+
+
 }
 
+void Menu::dispalyRegister()
+{
+
+    std::string regdisplay
+    {
+      "\t_____________________________________________\n"
+      "\t| \tID | \tGroup ID | \tName | \tAttendance |\n"
+    };
+}
